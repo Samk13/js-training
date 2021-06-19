b7f04c71957e24d922760637a34cf80da741d5fe
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2021-06-19 17:07:57 +0200

    exec 9

--- src/exo09.js
+++ src/exo09.js
@@ -3,9 +3,9 @@ export function parseUserData(data) {
   // et ces valeurs par défaut pour les propriétés manquantes :
   const defaults = { name: "Anonymous", isAdmin: false };
 
+  return Object.assign(defaults, data);
+
   // 1 - en utilisant Object.assign
   // 2 - en utilisant l'opérateur spread sur les properties
   // 3 - en utilisant le destructuring et les paramètres par défaut pour parseUserData
-
-  return data;
 }
